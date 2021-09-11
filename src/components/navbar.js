647ce6b87a5328151aa8b7504f0823ba4a88176c
vipin <chaudharyv714@gmail.com>
2021-09-12 03:56:06 +0530

    dc

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,20 +1,37 @@
 import '../App.css';
+import React, { useState } from 'react';
 
-function navbar() {
-    return (
-      <div className="nav">
-          <div className="listcontainer">
-              <ul>
-               <li><a href="#">Home</a></li>
-               <li><a href="#">About</a></li>
-               <li><a href="#">Topics</a></li>
-               <li><a href="#">Schedule</a></li>
-               <li><a href="#">Committee</a></li>
-               <li><a href="#">Contact</a></li>  
-              </ul>
-          </div>
+
+function Navbar() {
+
+  const [flag, updateflag] = useState("listcontainer off");
+
+  var callupdateflag = () => {
+    updateflag((prevflag) =>{ 
+    if(prevflag=="listcontainer on")
+      return "listcontainer off";
+      return "listcontainer on";
+    });
+  };
+
+
+  return (
+    <div className="nav">
+      <button onClick={callupdateflag} className="menu">
+        <i className="fas fa-bars"></i><span>Chemical</span>
+      </button>
+      <div className={flag}>
+        <ul>
+          <li><a href="#">Home</a></li>
+          <li><a href="#">About</a></li>
+          <li><a href="#">Topics</a></li>
+          <li><a href="#">Schedule</a></li>
+          <li><a href="#">Committee</a></li>
+          <li><a href="#">Contact</a></li>
+        </ul>
       </div>
-    );
-  }
- 
-  export default navbar;
+    </div>
+  );
+}
+
+export default Navbar;
